4530496d3730e1d3212885f71d2a2db7e7e7ec69
MD Khairul Islam <mkiuzzal07@gmail.com>
2025-05-19 16:22:03 +0600

    initial setup for practice redux

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { decrement, increment } from "./redux/features/counter/counter";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <div className="flex justify-between items-center h-screen">
+      <h1>Counter with Redux</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
